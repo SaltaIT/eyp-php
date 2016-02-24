aae94fd55444946eed2ffac15ca6b7c4c6f7a05b
Armando Gil <armando.gil@ntt.eu>
2016-02-24 13:24:42 +0100

    Solved almost all the rake lint issues

--- manifests/fpm.pp
+++ manifests/fpm.pp
@@ -1,84 +1,84 @@
 define php::fpm (
-			$instancename=$name,
-			$confbase=$php::params::confbase_fpm,
-			$errorlog='/var/log/php5-fpm.log',
-			$loglevel='notice',
-			$processmax=0,
-			$processpriority=-19,
-			$user=$php::params::user,
-			$group=$php::params::group,
-			$exposephp="Off",
-			$maxexecutiontime="5",
-			$memorylimit="10M",
-			$maxupload="100M",
-			$maxpostsize="100M",
-			$timezone="Europe/Andorra",
-			$allowurlfopen="Off",
-			$allowurlinclude="Off",
-			$customini=undef,
-		) {
+  $instancename=$name,
+  $confbase=$php::params::confbase_fpm,
+  $errorlog='/var/log/php5-fpm.log',
+  $php_loglevel='notice',
+  $processmax=0,
+  $processpriority=-19,
+  $user=$php::params::user,
+  $group=$php::params::group,
+  $exposephp='Off',
+  $maxexecutiontime='5',
+  $memorylimit='10M',
+  $maxupload='100M',
+  $maxpostsize='100M',
+  $timezone='Europe/Andorra',
+  $allowurlfopen='Off',
+  $allowurlinclude='Off',
+  $customini=undef,
+  ) {
 
-	if defined(Class['ntteam'])
-	{
-		ntteam::tag{ 'php::fpm': }
-	}
+  if defined(Class['ntteam'])
+  {
+  ntteam::tag{ 'php::fpm': }
+  }
 
-	validate_absolute_path($confbase)
-	validate_absolute_path($errorlog)
+  validate_absolute_path($confbase)
+  validate_absolute_path($errorlog)
 
-	validate_re($exposephp, '^O(n|ff)$', "Not a valid option")
-	validate_re($allowurlfopen, '^O(n|ff)$', "Not a valid option")
-	validate_re($allowurlinclude, '^O(n|ff)$', "Not a valid option")
+  validate_re($exposephp, '^O(n|ff)$', 'Not a valid option')
+  validate_re($allowurlfopen, '^O(n|ff)$', 'Not a valid option')
+  validate_re($allowurlinclude, '^O(n|ff)$', 'Not a valid option')
 
-	validate_re($loglevel, [ '^alert$', '^error$', '^warning$', '^notice$', '^debug$' ], "Not a valid loglevel: $loglevel")
+  validate_re($php_loglevel, [ '^alert$', '^error$', '^warning$', '^notice$', '^debug$' ], "Not a valid loglevel: ${php_loglevel}")
 
-	validate_integer($processmax, 0)
-	validate_integer($processpriority, 20, -19)
+  validate_integer($processmax, 0)
+  validate_integer($processpriority, 20, -19)
 
-	if defined(Class['ntteam'])
-	{
-		ntteam::tag{ 'phpfpm': }
-	}
+  if defined(Class['ntteam'])
+  {
+  ntteam::tag{ 'phpfpm': }
+  }
 
-	package { $php::params::phpfpmpackage:
-		ensure => 'installed',
-	}
+  package { $php::params::phpfpmpackage:
+  ensure => 'installed',
+  }
 
-	file { "${confbase}/php-fpm.conf":
-		ensure => 'present',
-		owner => 'root',
-		group => 'root',
-		mode => '0644',
-		content => template("php/phpfpmconf.erb"),
-		notify => Service['php5-fpm'],
-		require => Package[$php::params::phpfpmpackage],
-	}
+  file { "${confbase}/php-fpm.conf":
+  ensure  => 'present',
+  owner   => 'root',
+  group   => 'root',
+  mode    => '0644',
+  content => template('php/phpfpmconf.erb'),
+  notify  => Service['php5-fpm'],
+  require => Package[$php::params::phpfpmpackage],
+  }
 
-	if($customini)
-	{
-		file { "${confbase}/php.ini":
-			ensure => $customini,
-			force => true,
-			notify => Service['php5-fpm'],
-		}
-	}
-	else
-	{
-		file { "${confbase}/php.ini":
-			ensure => 'present',
-			owner => 'root',
-			group => 'root',
-			mode => '0644',
-			content => template("php/phpini.erb"),
-			notify => Service['php5-fpm'],
-			require => Package[$php::params::phpfpmpackage],
-		}
-	}
+  if($customini)
+  {
+  file { "${confbase}/php.ini":
+  ensure => $customini,
+  force  => true,
+  notify => Service['php5-fpm'],
+  }
+  }
+  else
+  {
+  file { "${confbase}/php.ini":
+  ensure  => 'present',
+  owner   => 'root',
+  group   => 'root',
+  mode    => '0644',
+  content => template('php/phpini.erb'),
+  notify  => Service['php5-fpm'],
+  require => Package[$php::params::phpfpmpackage],
+  }
+  }
 
-	#TODO:rewrite for multiple daemon
-	service {'php5-fpm':
-		enable => true,
-		ensure => "running",
-		require => File[ [ "${confbase}/php-fpm.conf", "${confbase}/php.ini" ] ],
-	}
+  #TODO:rewrite for multiple daemon
+  service {'php5-fpm':
+  ensure  => 'running',
+  enable  => true,
+  require => File[ [ "${confbase}/php-fpm.conf", "${confbase}/php.ini" ] ],
+  }
 }
--- manifests/params.pp
+++ manifests/params.pp
@@ -1,35 +1,39 @@
 class php::params () {
 
-	case $::osfamily
-	{
-		'Debian':
-		{
-			case $::operatingsystem
-			{
-				'Ubuntu':
-				{
-					case $::operatingsystemrelease
-					{
-						/^14.*$/:
-						{
-							$phpdependencies=['php-pear']
-							$phpfpmpackage=[ 'php5-fpm', 'libfcgi0ldbl' ]
-							$phpcli=[ 'php5-cli' ]
-							$user="www-data"
-							$group="www-data"
-							$confbase="/etc/php5/"
-							$confbase_cli="/etc/php5/cli"
-							$confbase_fpm="/etc/php5/fpm"
-							$pecl_dependencies=['php5-dev']
-						}
-						default: { fail("Unsupported Ubuntu version! - $::operatingsystemrelease")  }
-					}
-				}
-				'Debian': { fail("Unsupported")  }
-				default: { fail("Unsupported Debian flavour!")  }
-			}
-		}
-		default: { fail("Unsupported OS!")  }
-	}
+        $phpapache_default=false
+
+  case $::osfamily
+  {
+    'Debian':
+    {
+      case $::operatingsystem
+      {
+      'Ubuntu':
+      {
+      case $::operatingsystemrelease
+      {
+        /^14.*$/:
+        {
+          $phpdependencies=['php-pear']
+          $phpfpmpackage=[ 'php5-fpm', 'libfcgi0ldbl' ]
+          $phpcli=[ 'php5-cli' ]
+          $phpapachepackage=[ 'libapache2-mod-php5' ]
+          $user='www-data'
+          $group='www-data'
+          $confbase='/etc/php5/'
+          $confbase_cli='/etc/php5/cli'
+          $confbase_fpm='/etc/php5/fpm'
+          $confbase_apache='/etc/php5/apache2'
+          $pecl_dependencies=['php5-dev']
+        }
+        default: { fail("Unsupported Ubuntu version! - ${::operatingsystemrelease}")  }
+      }
+      }
+      'Debian': { fail('Unsupported')  }
+      default: { fail('Unsupported Debian flavour!')  }
+      }
+    }
+    default: { fail('Unsupported OS!')  }
+  }
 
 }
