cb4816e315dc32a5804608d0d8f88c9af45b5e58
Jordi Prats <jprats@systemadmin.es>
2017-01-25 10:28:22 +0100

    bugfix os

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,2 +1,6 @@
 # CHANGELOG
 
+## 0.2.6
+
+* minor changes: lint
+* bugfix OS detection
--- manifests/params.pp
+++ manifests/params.pp
@@ -25,31 +25,30 @@ class php::params () {
 
   case $::osfamily
   {
-    'Debian':
+    'redhat':
+    {
+      #TODO: definir valors per centos
+      $phpdependencies=['php']
+      $phpfpmpackage=[ 'php-fpm' ]
+      $phpcli=[ 'php-cli' ]
+      $user='apache'
+      $group='apache'
+      $confbase='/etc/php5/'
+      $confbase_cli='/etc/php5/cli'
+      $confbase_fpm='/etc/php5/fpm'
+      $pecl_dependencies=['php5-dev']
+
+      case $::operatingsystemrelease
+      {
+        /^[5-7].*$/:
+        {
+        }
+        default: { fail('Unsupported RHEL/CentOS version!')  }
+      }
+    }
     {
       case $::operatingsystem
       {
-        'redhat':
-        {
-          #TODO: definir valors per centos
-          $phpdependencies=['php']
-          $phpfpmpackage=[ 'php-fpm' ]
-          $phpcli=[ 'php-cli' ]
-          $user='apache'
-          $group='apache'
-          $confbase='/etc/php5/'
-          $confbase_cli='/etc/php5/cli'
-          $confbase_fpm='/etc/php5/fpm'
-          $pecl_dependencies=['php5-dev']
-
-          case $::operatingsystemrelease
-          {
-            /^[5-7].*$/:
-            {
-            }
-            default: { fail('Unsupported RHEL/CentOS version!')  }
-          }
-        }
         'Ubuntu':
         {
           case $::operatingsystemrelease
