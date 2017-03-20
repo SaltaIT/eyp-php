0196bfdebf4de2f4af8e6d03897385e0eef027d5
Jordi Prats <jprats@systemadmin.es>
2017-03-20 11:27:19 +0100

    master i slave mysqlnd_ms

---
+++ manifests/mysqlnd_ms/master.pp
@@ -0,0 +1,13 @@
+define php::mysqlnd_ms::master(
+                                $datasource_name,
+                                $host_name = $name,
+                                $host      = $name,
+                                $port      = '3306',
+                                $socket    = undef,
+                              ) {
+  concat::fragment{ "${php::params::confbase}/mysqlndms.conf nd_ms master ${host_name} ${datasource_name}":
+    target  => "${php::params::confbase}/mysqlndms.conf".
+    order   => "10-${datasource_name}-02",
+    content => template("${module_name}/mysqlnd/ms/host.erb")
+  }
+}
---
+++ manifests/mysqlnd_ms/slave.pp
@@ -0,0 +1,13 @@
+define php::mysqlnd_ms::slave(
+                                $datasource_name,
+                                $host_name = $name,
+                                $host      = $name,
+                                $port      = '3306',
+                                $socket    = undef,
+                              ) {
+  concat::fragment{ "${php::params::confbase}/mysqlndms.conf nd_ms slave ${host_name} ${datasource_name}":
+    target  => "${php::params::confbase}/mysqlndms.conf".
+    order   => "10-${datasource_name}-05",
+    content => template("${module_name}/mysqlnd/ms/host.erb")
+  }
+}
