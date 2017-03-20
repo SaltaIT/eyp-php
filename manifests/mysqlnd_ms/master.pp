3077d8a159a05e34d4b38ba8c040ec50b1dd8aba
Jordi Prats <jprats@systemadmin.es>
2017-03-20 11:32:55 +0100

    lint

--- manifests/mysqlnd_ms/master.pp
+++ manifests/mysqlnd_ms/master.pp
@@ -6,7 +6,7 @@ define php::mysqlnd_ms::master(
                                 $socket    = undef,
                               ) {
   concat::fragment{ "${php::params::confbase}/mysqlndms.conf nd_ms master ${host_name} ${datasource_name}":
-    target  => "${php::params::confbase}/mysqlndms.conf".
+    target  => "${php::params::confbase}/mysqlndms.conf",
     order   => "10-${datasource_name}-02",
     content => template("${module_name}/mysqlnd/ms/host.erb")
   }
--- manifests/mysqlnd_ms/slave.pp
+++ manifests/mysqlnd_ms/slave.pp
@@ -6,7 +6,7 @@ define php::mysqlnd_ms::slave(
                                 $socket    = undef,
                               ) {
   concat::fragment{ "${php::params::confbase}/mysqlndms.conf nd_ms slave ${host_name} ${datasource_name}":
-    target  => "${php::params::confbase}/mysqlndms.conf".
+    target  => "${php::params::confbase}/mysqlndms.conf",
     order   => "10-${datasource_name}-05",
     content => template("${module_name}/mysqlnd/ms/host.erb")
   }
