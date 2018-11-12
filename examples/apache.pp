d2880342f5173bf771a2fe8ddecf32b72147a4ba
Jordi Prats <jordi.prats@gmail.com>
2018-11-12 14:26:36 +0100

    dependencia apache

--- examples/apache.pp
+++ examples/apache.pp
@@ -1,2 +1,10 @@
 
+class { 'apache':
+  listen                => [ '80', '443' ],
+  ssl                   => true,
+  manage_docker_service => true,
+}
+
+class { 'apache::mod::php': }
+
 class { 'php::apache': }
